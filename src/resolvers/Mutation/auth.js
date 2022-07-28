d55b4a1f22afc503b43c856e9c88a5e0d73c00d0
James <metal.lunchbox@gmail.com>
2022-07-28 23:29:57 +0800

    use node:crypto instead of bcrypt library

--- src/resolvers/Mutation/auth.js
+++ src/resolvers/Mutation/auth.js
@@ -1,13 +1,29 @@
-const { hash, compare } = require('bcrypt')
+// const { hash, compare } = require('bcrypt')
+const { scryptSync, randomBytes } = require('node:crypto')
 const { sign } = require('jsonwebtoken')
 const { isValidEmail } = require('../../utils')
+
+const encryptPassword = (password, salt) => {
+  return scryptSync(password, salt, 32).toString('hex')
+}
+const hashPassword = password => {
+  const salt = randomBytes(16).toString('hex')
+  return encryptPassword(password, salt) + salt
+}
+const passwordMatches = (password, hash) => {
+  const salt = hash.slice(64)
+  const originalPassHash = hash.slice(0, 64)
+  const currentPassHash = encryptPassword(password, salt)
+  return originalPassHash === currentPassHash
+}
 
 module.exports.auth = {
   createUser: async (_, parameters, context) => {
     if (parameters.email && !isValidEmail(parameters.email)) {
       throw new Error('email address not valid')
     }
-    const hashedPassword = await hash(parameters.password, 10)
+    // const hashedPassword = await hash(parameters.password, 10)
+    const hashedPassword = hashPassword(parameters.password)
     const user = await context.prisma.user.create({
       data: { ...parameters, password: hashedPassword },
     })
@@ -26,8 +42,7 @@ module.exports.auth = {
     if (!user) {
       throw new Error(`No user found for username: ${username}`)
     }
-    const passwordValid = await compare(password, user.password)
-    if (!passwordValid) {
+    if (!passwordMatches(password, user.password)) {
       throw new Error('Invalid password')
     }
     return {
